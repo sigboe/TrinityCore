591733ba4c7e7e65ee242eeba66d5d4da68eb3d3
ulmetrs <robertulmet@gmail.com>
2025-07-30 11:09:33 -0400

    Revert "test map splitting UpdatePositionData changes"
    This reverts commit b35096bd423eeaaa42b8df4e56e7959b0043ebc8.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13804,10 +13804,9 @@ bool Unit::UpdatePosition(float x, float y, float z, float orientation, bool tel
             GetMap()->CreatureRelocation(ToCreature(), x, y, z, orientation);
     }
     else if (turn)
-    {
         UpdateOrientation(orientation);
-        UpdatePositionData();
-    }
+
+    UpdatePositionData();
 
     _positionUpdateInfo.Relocated = relocated;
     _positionUpdateInfo.Turned = turn;
