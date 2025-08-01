ddb90cae2b737f436a5477e68555d38785dd5a63
ulmetrs <robertulmet@gmail.com>
2025-08-01 10:58:56 -0400

    remove

--- src/server/game/World/World.h
+++ src/server/game/World/World.h
@@ -696,8 +696,6 @@ class TC_GAME_API World
         /// Active session server limit
         void SetPlayerAmountLimit(uint32 limit) { m_playerLimit = limit; }
         uint32 GetPlayerAmountLimit() const { return m_playerLimit; }
-        void SetPlayerAmountLimitNoQueue(bool noQueue) { m_playerLimitNoQueue = noQueue; }
-        bool GetPlayerAmountLimitNoQueue() const { return m_playerLimitNoQueue; }
 
         //player Queue
         typedef std::list<WorldSession*> Queue;
@@ -910,7 +908,6 @@ class TC_GAME_API World
         typedef std::map<uint32, uint64> WorldStatesMap;
         WorldStatesMap m_worldstates;
         uint32 m_playerLimit;
-        bool m_playerLimitNoQueue;
         AccountTypes m_allowedSecurityLevel;
         LocaleConstant m_defaultDbcLocale;                     // from config for one from loaded DBC locales
         uint32 m_availableDbcLocaleMask;                       // by loaded DBC
