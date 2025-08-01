3e9bfd5a02cb7828d0702dd90419ec5638f53a95
ulmetrs <robertulmet@gmail.com>
2025-08-01 13:30:54 -0400

    Revert "Update World.cpp"
    This reverts commit c35ea6377d273087b0ecb5ab3f773e8e16799133.

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -372,7 +372,7 @@ void World::AddSession_(WorldSession* s)
         --Sessions;
 
     uint32 loadingSessions = GetLoadingSessionCount();
-    constexpr uint32 MAX_LOADING_SESSIONS = 5;
+    constexpr uint32 MAX_LOADING_SESSIONS = 100;
     
     if (!pLimitNoQueue && ((pLimit > 0 && Sessions >= pLimit) || loadingSessions >= MAX_LOADING_SESSIONS) && !s->HasPermission(rbac::RBAC_PERM_SKIP_QUEUE) && !HasRecentlyDisconnected(s))
     {
