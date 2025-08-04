6345441802670363f4da196bee335076301237fb
ulmetrs <robertulmet@gmail.com>
2025-08-04 04:24:56 -0400

    Update World.cpp

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -3330,14 +3330,9 @@ void World::UpdateSessions(uint32 diff)
             
             while (!m_QueuedPlayer.empty() && processedPlayers < maxPlayersToProcess)
             {
-                // Check player limit
-                uint32 sessions = GetActiveSessionCount();
-                if (m_playerLimit && sessions >= m_playerLimit)
-                    break;
-                
                 WorldSession* session = m_QueuedPlayer.front();
                 m_QueuedPlayer.pop_front();
-                
+
                 session->InitializeSession();
                 UpdateMaxSessionCounters();
                 
